a12d398acbf81cedf81647b83a6630e7a87cc197
wangpengsen <19317362@qq.com>
2021-01-14 13:51:41 +0800

    docker for ndk-r22 done

--- base/bld.sh
+++ base/bld.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+# usage:
+#  docker run -d --name wps-alp -p 2222:22 cd-base
+#  user:root
+#  pwd:admin
+#   ssh -p 2222 root@127.0.0.1 
 docker rmi cd-base
 docker build -t cd-base:latest .
--- ndkr22/bld.sh
+++ ndkr22/bld.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# usage:
+#  docker run -d --name wps-ndk-r22 -p 2222:22 cd-ndk:r22 
+#  user:root
+#  pwd:admin
+#   ssh -p 2222 root@127.0.0.1 
+
 docker build -t cd-ndk:r22 .
